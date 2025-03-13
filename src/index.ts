82faeeaaf71a6455f9fe83b9fffe10b61345713e
redaphid <iam@aaronherres.com>
2025-03-12 17:05:28 -0700

    websocket server does something

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,45 @@
-import { DurableObject } from 'cloudflare:workers'
-import { z } from 'zod'
+/// <reference types="@cloudflare/workers-types" />
+
+import { DurableObject, DurableObjectNamespace, DurableObjectState } from '@cloudflare/workers-types'
+
 export interface Env {
-  WEBSOCKET_SERVER: DurableObjectNamespace<WebSocketServer>
-  FIREHOUSE_FRENZY: KVNamespace
+  WEBSOCKET_SERVER: DurableObjectNamespace
   ASSETS: any
 }
 
 // Worker
 export default {
-  async fetch(request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const id = env.WEBSOCKET_SERVER.idFromName('foo')
+  async fetch(request: Request, env: Env) {
+    const id = env.WEBSOCKET_SERVER.idFromName('funky-firehose')
     const stub = env.WEBSOCKET_SERVER.get(id)
+    // if we have an upgrade header, assume we're trying to connect to the websocket
+    if (request.headers.get('Upgrade') === 'websocket') return stub.fetch(request)
+
     if (request.method === 'POST') {
-      const body = await request.json()
-      const parsed = firehoseSchema.parse(body)
-      const address = formatAddress(parsed.data.propertyAddress)
-      const coords = await addressToCoords(address, env)
+      // const body = await request.json()
 
       await stub.fetch('https://internal/broadcast', {
         method: 'POST',
-        body: JSON.stringify({ coords }),
+        body: JSON.stringify({ message: 'did you get this?' }),
         headers: { topic: request.headers.get('topic') ?? 'unknown-topic' },
       })
 
       return new Response(null, { status: 204 })
     }
 
-    // if we have an upgrade header, assume we're trying to connect to the websocket
-    if (request.headers.get('Upgrade') === 'websocket') return stub.fetch(request)
-
     return env.ASSETS.fetch(request)
   },
 }
 
 // Durable Object
-export class WebSocketServer extends DurableObject {
-  sessions: Set<WebSocket>
-  state: DurableObjectState
-  constructor(ctx: DurableObjectState, env: Env) {
-    super(ctx, env)
-    this.sessions = new Set()
-    this.state = ctx
-    this.state.getWebSockets().forEach(this.sessions.add)
+export class WebSocketServer implements DurableObject {
+  private sessions = new Set<WebSocket>()
+
+  constructor(private state: DurableObjectState) {
+    this.state.getWebSockets().forEach((ws) => this.sessions.add(ws))
   }
 
-  async fetch(request: Request) {
+  async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url)
     if (url.pathname === '/broadcast') {
       return this.broadcast({
@@ -56,14 +51,13 @@ export class WebSocketServer extends DurableObject {
     }
 
     // WebSocket connection handling
-    const webSocketPair = new WebSocketPair()
-    const [client, server] = Object.values(webSocketPair)
+    const [client, server] = Object.values(new WebSocketPair())
 
     server.accept()
     this.sessions.add(server)
 
-    server.addEventListener('close', (cls: CloseEvent) => {
-      server.close(cls.code, 'Durable Object is closing WebSocket')
+    server.addEventListener('close', () => {
+      server.close()
       this.sessions.delete(server)
     })
 
@@ -73,8 +67,8 @@ export class WebSocketServer extends DurableObject {
     })
   }
 
-  broadcast(message) {
-    message = this.sessions.forEach((webSocket) => {
+  broadcast(message: any): Response {
+    this.sessions.forEach((webSocket) => {
       try {
         webSocket.send(JSON.stringify(message))
       } catch (err) {
@@ -84,47 +78,4 @@ export class WebSocketServer extends DurableObject {
 
     return new Response(null, { status: 204 })
   }
-}
-
-const addressToCoords = async (address: string, env: Env) => {
-  const coordFromKv = await env.FIREHOUSE_FRENZY.get(address)
-  if (coordFromKv) return JSON.parse(coordFromKv)
-  const coord = await fetch(`https://nominatim.openstreetmap.org/search.php?q=${address}&format=jsonv2`, {
-    headers: {
-      'User-Agent': 'sibi-firehouse-frenzy',
-    },
-  })
-  const parsed = nominatimSchema.parse(await coord.json())
-  const coords = {
-    lat: parsed[0].lat,
-    lon: parsed[0].lon,
-  }
-  await env.FIREHOUSE_FRENZY.put(address, JSON.stringify(coords))
-  return coords
-}
-
-const nominatimSchema = z.array(
-  z.object({
-    lat: z.string(),
-    lon: z.string(),
-  }),
-)
-
-const firehoseSchema = z.object({
-  data: z.object({
-    topic: z.string().optional(),
-    propertyAddress: z.object({
-      line1: z.string(),
-      line2: z.string().optional(),
-      city: z.string(),
-      stateOrProvince: z.string(),
-      postalCode: z.string(),
-    }),
-  }),
-})
-
-type FirehoseMessage = z.infer<typeof firehoseSchema>
-
-const formatAddress = (address: FirehoseMessage['data']['propertyAddress']): string => {
-  return [address.line1, address.line2, address.city, address.stateOrProvince, address.postalCode].filter(Boolean).join(' ')
 }
